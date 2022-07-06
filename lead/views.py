e2db6b643704cfe661a898f04d6bb0cda3f8b2bb
TarKz06 <khematat_s@elearning.cmu.ac.th>
2022-07-06 14:44:26 +0700

    Cerate laeding app

---
+++ lead/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets
+
+from team.models import Team
+
+from .models import Lead
+from .serializers import LeadSerializer
+
+class LeadViewSet(viewsets.ModelViewSet):
+    serializer_class = LeadSerializer
+    queryset = Lead.objects.all()
+    
+    def perform_create(self, serializer):
+        team = Team.objects.filter(members__in=[self.request.user]).first()
+
+        serializer.save(team=team, created_by=self.request.user)
+
+    def get_queryset(self):
+        team = Team.objects.filter(members__in=[self.request.user]).first()
+
+        return self.queryset.filter(team=team)
